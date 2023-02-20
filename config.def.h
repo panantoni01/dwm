8c3633e188f13bc9c961c2b80de8e98fb0b23577
Antoni Pokusiński <apokusinski@o2.pl>
2023-02-20 15:19:44 +0100

    config: add descriptions, customize gappx

--- config.def.h
+++ config.def.h
@@ -1,16 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gap size between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
+static const char col_gray1[]       = "#222222"; /* background color */
+static const char col_gray2[]       = "#444444"; /* inactive window border color */
+static const char col_gray3[]       = "#bbbbbb"; /* font color */
+static const char col_gray4[]       = "#eeeeee"; /* current tag and current window font color */
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
