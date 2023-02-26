6742feed7400592f1d6cf2f791bbef6340ebbe60
Antoni Pokusiński <apokusinski@o2.pl>
2023-02-26 17:51:50 +0100

    vscodecmd: disable gpu

--- config.def.h
+++ config.def.h
@@ -68,7 +68,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]     = { "terminator", NULL };
 static const char *browsercmd[]  = { "opera", NULL };
-static const char *vscodecmd[]   = { "code", NULL };
+static const char *vscodecmd[]   = { "code", "--disable-gpu", NULL };
 static const char *discordcmd[]  = { "discord", NULL };
 
 /* commands */
