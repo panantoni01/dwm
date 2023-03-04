0e949e50fd48318f6006cbd7ff3b21363a8a0144
Antoni Pokusiński <apokusinski@o2.pl>
2023-03-04 22:33:46 +0100

    Switch from terminator to st

--- config.def.h
+++ config.def.h
@@ -8,6 +8,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=13",  "FontAwesome5Brands:size=13:antialias:true", "FontAwesome5Free:size=13:antialias:true", "FontAwesome5Free:style=Solid:size=13:antialias:true" };
 static const char dmenufont[]       = "monospace:size=13";
+static const char alpha[]           = "0.8";     /* background opacity */
+static const char termfont[]        = "monospace:size=13";
 static const char col_gray1[]       = "#222222"; /* background color */
 static const char col_gray2[]       = "#444444"; /* inactive window border color */
 static const char col_gray3[]       = "#bbbbbb"; /* font color */
@@ -17,6 +19,7 @@ static const char col_black[]       = "#000000";
 static const char col_red[]         = "#ff0000";
 static const char col_yellow[]      = "#ffff00";
 static const char col_white[]       = "#ffffff";
+
 
 static const char *colors[][3]      = {
 	/*					fg         bg          border   */
@@ -66,7 +69,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]     = { "terminator", NULL };
+static const char *termcmd[]     = { "st", "-A", alpha, "-f", termfont,  NULL };
 static const char *browsercmd[]  = { "opera", NULL };
 static const char *vscodecmd[]   = { "code", "--disable-gpu", NULL };
 static const char *discordcmd[]  = { "discord", NULL };
